90dae72978233d57d777bfb76e2d7f429446d4d7
Bustracker <bustracker94@gmail.com>
2025-10-30 21:43:51 +0530

    fix: add CORS headers

--- Chitchat/index.js
+++ Chitchat/index.js
@@ -4,6 +4,10 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
+app.use(cors({
+  origin: [FRONTEND_URL, 'https://chatapplication-dusky.vercel.app', 'https://chatapplication-indol-nine.vercel.app'],
+  credentials: true
+}));
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
